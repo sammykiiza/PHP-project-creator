786c09152213059e08dc8ebb2e801e468b74dd08
sammykiiza <sammykiiza@gmail.com>
2022-03-26 21:45:29 +0300

    added composer support

--- src/innerFiles/innerProjectFiles.php
+++ src/innerFiles/innerProjectFiles.php
@@ -17,15 +17,15 @@ class InnerProjectFiles
     public function create(): void
     {
         $command1 = "chmod +rwx {$this->currentDir}/{$this->name}";
-        exec($command1);
 
         $thisFilePath = realpath(dirname(__FILE__));
 
         $command2 = "cat $thisFilePath/fileContents/index.php > $this->currentDir/$this->name/index.php";
-        exec($command2);
 
         $command3 = "mkdir -v -p {$this->currentDir}/{$this->name}/src {$this->currentDir}/{$this->name}/src/Entities {$this->currentDir}/{$this->name}/src/Repositories";
-        exec($command3);
+
+        exec($command1."&&".$command2."&&".$command3."&& cd "
+        .$this->currentDir."/".$this->name."&& composer init");
     }
     
 }
